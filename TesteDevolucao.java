ec1d6813c994337c31f51f677b5a2f475ace535d
Juci P Leal <juportoleal@gmail.com>
2021-05-14 03:35:47 -0300

    lapidado --> testar com o grupo

--- TesteDevolucao.java
+++ TesteDevolucao.java
@@ -1,19 +1,33 @@
 package prjModuloBloco1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TesteDevolucao{
 
-	public static void TesteDevolucao() {
-	//public static void main(String[] args) {
+	public static void devolucao() {
 		// TODO Auto-generated method stub
-		int codCliente;
+		int codCliente = 0;
 
 		Scanner leia = new Scanner(System.in);
 		
 		System.out.println("\t\n::: Devolução de Livros ::: \n");
-		System.out.println("Código Cliente: ");
-		codCliente = leia.nextInt();
+		boolean continueLoop = true;
+		do {
+			try {		
+				System.out.println("Código do cliente: ");
+				codCliente = leia.nextInt();
+				continueLoop = false;
+			}
+			
+			catch(InputMismatchException e) {
+				//System.err.printf("\nExceptption: %s\n",e);
+				leia.nextLine();
+				System.out.println("\nDigite um valor do tipo inteiro. Por favor, tente novamente.\n");
+			}
+			
+		}while(continueLoop);
+		
 		EmprestimoDevolucao devolucao = new EmprestimoDevolucao(codCliente);
 		devolucao.devolverLivro();
 	}
